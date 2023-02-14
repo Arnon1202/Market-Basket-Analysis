e3ce23a54935791c3929751b366c9fdb95a59935
Arnon1202 <39861269+Arnon1202@users.noreply.github.com>
2023-02-14 20:35:30 +0000

    Pending changes exported from your codespace

---
+++ market-basket-analysis/analysis.py
@@ -0,0 +1,9 @@
+#importing the required libraries.
+import numpy as np
+import pandas as pd
+import matplotlib as mpt
+
+
+#function for reading the data from the files.
+def of_read_data(file_num):
+    
